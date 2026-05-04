6aaf04087a61f8871931462902466850c463a1ad
Mehathab06 <shaikmehathab20@gmail.com>
2026-05-04 15:23:12 +0530

    commited

---
+++ Average.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Average {
+    public static void main(String[] args){
+        int num1 = 32;
+        int num2 = 26;
+        int num3 = 41;
+                int num4 = 55;
+        int AverageOfThreeNumbers = num1 + num2+ num3 + num4 / 4;
+        System.out.println(AverageOfThreeNumbers);
+
+    }
+}
